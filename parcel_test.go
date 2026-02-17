525eb83e71e117c884a7b64cdd973ceb67da7795
Sergey Timofeev <tsa89161804999@gmail.com>
2026-02-17 06:23:04 +0300

    Исправлено по комментариям ревьюера Ивана Нижниченко

--- parcel_test.go
+++ parcel_test.go
@@ -51,11 +51,7 @@ func TestAddGetDelete(t *testing.T) {
 	require.NoError(t, err)
 
 	// проверьте, что значения всех полей в полученном объекте совпадают со значениями полей в переменной parcel
-	require.Equal(t, parcel.Number, storedParcel.Number)
-	require.Equal(t, parcel.Client, storedParcel.Client)
-	require.Equal(t, parcel.Status, storedParcel.Status)
-	require.Equal(t, parcel.Address, storedParcel.Address)
-	require.Equal(t, parcel.CreatedAt, storedParcel.CreatedAt)
+	require.Equal(t, parcel, storedParcel)
 
 
 	// delete
@@ -174,10 +170,6 @@ func TestGetByClient(t *testing.T) {
 		expectedParcel, ok := parcelMap[parcel.Number]
 		require.True(t, ok)
 		// убедитесь, что значения полей полученных посылок заполнены верно
-		require.Equal(t, expectedParcel.Number, parcel.Number)
-		require.Equal(t, expectedParcel.Client, parcel.Client)
-		require.Equal(t, expectedParcel.Status, parcel.Status)
-		require.Equal(t, expectedParcel.Address, parcel.Address)
-		require.Equal(t, expectedParcel.CreatedAt, parcel.CreatedAt)
+		require.Equal(t, expectedParcel, parcel)
 	}
 }
